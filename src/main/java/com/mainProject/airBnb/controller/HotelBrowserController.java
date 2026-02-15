db9324ffc4b729e9dc4e279eba0d6e4ae2b4e2fc
Umang Jain <ujain7315@gmail.com>
2026-02-15 23:56:33 +0530

    making a search functionality

--- src/main/java/com/mainProject/airBnb/controller/HotelBrowserController.java
+++ src/main/java/com/mainProject/airBnb/controller/HotelBrowserController.java
@@ -1,12 +1,32 @@
 package com.mainProject.airBnb.controller;
 
 
+import com.mainProject.airBnb.dto.HotelDTO;
+import com.mainProject.airBnb.dto.HotelSearchDto;
+import com.mainProject.airBnb.entity.Hotel;
+import com.mainProject.airBnb.service.InventorySVC;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/hotels")
 public class HotelBrowserController {
+
+    private final InventorySVC is;
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<HotelDTO>> searchHotel(@RequestBody HotelSearchDto Hotelsearch){
+        Page<HotelDTO> page = is.searchHotels(hotelSearch);
+
+
+        return page;
+    }
 }
